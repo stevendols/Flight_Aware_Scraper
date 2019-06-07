1baaf158c3b6b0182af834b439656e631277aaab
Steven Dols <stevendols5798@pointloma.edu>
2019-06-07 09:06:44 -0700

    Gather and Store Plane Identifier (N-Number or Flight Call Sign)

--- src/FlightAware.java
+++ src/FlightAware.java
@@ -7,18 +7,24 @@ public class FlightAware
 {
     public static void main(String[] args)
     {
-        Document planePage;
+        Document planePage = null;
+        String planeID;
+
         try
         {
             planePage = Jsoup.connect("https://flightaware.com/live/flight/CAV470").get( );
             planePage = Jsoup.connect(planePage.location( ) + "/history").get( );
-
-            //test connection - debug only
-            System.out.println(planePage.location( ));
         }
         catch (IOException e)
         {
             e.printStackTrace( );
+            System.err.println("Plane Not Found, Program Will Exit");
+            System.exit(-1);
         }
+
+        //get plane ID (N-Number or flight identifier)
+        //ID is the first word in a 4 word string
+        planeID = planePage.select("td[align=left] h3").text( ).split(" ")[0];
+        System.out.println(planeID);
     }
 }
