73c52c7d5e6466e65e7d56baf5a559fef3fe3e45
Steven Dols <stevendols5798@pointloma.edu>
2019-06-07 08:53:21 -0700

    Connect to Flight Aware Website - page for history of a random flight

--- src/FlightAware.java
+++ src/FlightAware.java
@@ -1,7 +1,24 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+import java.io.IOException;
+
 public class FlightAware
 {
     public static void main(String[] args)
     {
+        Document planePage;
+        try
+        {
+            planePage = Jsoup.connect("https://flightaware.com/live/flight/CAV470").get( );
+            planePage = Jsoup.connect(planePage.location( ) + "/history").get( );
 
+            //test connection - debug only
+            System.out.println(planePage.location( ));
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace( );
+        }
     }
 }
